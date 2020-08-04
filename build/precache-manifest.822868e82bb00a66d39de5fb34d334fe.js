b189b85885ec1ae72626cddee45ff91f7871acc5
aftabchaudhary <aachaudhary1974@gmail.com>
2020-08-04 15:57:55 +0100

    first-commit

---
+++ build/precache-manifest.822868e82bb00a66d39de5fb34d334fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08082159928cf6783baa100f488942fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a041cb23ea3bc5121606",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "79855165f06c6de6b6de",
+    "url": "/static/js/2.6ea36e5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6ea36e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a041cb23ea3bc5121606",
+    "url": "/static/js/main.58719632.chunk.js"
+  },
+  {
+    "revision": "b4a24316a17c69e9b2dc",
+    "url": "/static/js/runtime-main.dece3cea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
